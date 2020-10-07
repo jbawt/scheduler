7a456bb4402f17f06f13f58f353705bd813a617f
Josh Bawtinheimer <jbawtinheimer@gmail.com>
2020-10-07 16:01:31 -0600

    add cypress tests

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,14 @@
+/* global cy */
+
+describe("Navigation", () => {
+  it("should visit root", () => {
+    cy.visit("/");
+  });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.css", "background-color", "rgb(242, 242, 242)");
+  });
+});
